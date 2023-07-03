c7f82fdeedc28e877b79553ed41d96bfad04ee3e
suhas-kadu <kadusuhas72@gmail.com>
2023-07-03 23:30:30 +0530

    name updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Sociosphere
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
